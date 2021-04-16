849e40363447504021d79846695c4235219a4c4b
PC <PC@DESKTOP-SCLFOG6>
2021-04-16 19:56:50 +0900

    The World!

--- Hello_Java/src/Hello_Java.java
+++ Hello_Java/src/Hello_Java.java
@@ -4,6 +4,7 @@ public class Hello_Java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java");
+		System.out.println("The World!!");
 
 	}
 
